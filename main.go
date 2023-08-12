a9cada1de9a267bd71b1fef0ed9e34f0a8de160a
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-08-12 21:47:42 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string = "Hello, Golang"
+	fmt.Println(message)
+
+	const pi float64 = 3.14159
+	fmt.Println("The value of pi is" , pi)
+
+
+
+
+
+}
+
