c1489d7d0804c52e79b29e8fd94a9c9adb24d0ff
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2023-08-13 07:20:59 +0530

    Create var_const.go

---
+++ var_const.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Variables
+	var message string = "Hello, Golang"
+	fmt.Println(message)
+
+	const pi float64 = 3.14159
+    fmt.Println("The value of pi is,", pi)
+
+
+
+}
